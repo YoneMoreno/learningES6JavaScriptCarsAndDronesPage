4940efe16c68e0e8848df0961df86a136f60a2a8
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-29 21:41:58 +0100

    Creating our first constructor

--- dist/src/app.js
+++ dist/src/app.js
@@ -1,11 +1,13 @@
-"use strict";
+'use strict';
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Drone = function Drone() {
-  _classCallCheck(this, Drone);
+var Drone = function Drone(id, name) {
+    _classCallCheck(this, Drone);
+
+    console.log('ID: ' + id + ' name: ' + name);
 };
 
-var drone = new Drone();
+var drone = new Drone('A123', 'Flyer');
 console.log(drone instanceof Drone);
 //# sourceMappingURL=app.js.map
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-class Drone{
-
+class Drone {
+    constructor(id, name) {
+        console.log('ID: ' + id + ' name: ' + name);
+    }
 }
 
-let drone = new Drone();
+let drone = new Drone('A123', 'Flyer');
 console.log(drone instanceof Drone);
