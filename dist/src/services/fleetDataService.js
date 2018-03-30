b0a531043354d78af3089f0d115c31df66d87a61
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-30 09:04:02 +0100

    We create a service class

---
+++ dist/src/services/fleetDataService.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.FleetDataService = undefined;
+
+var _Car = require("../classes/Car.js");
+
+var _Drone = require("../classes/Drone.js");
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var FleetDataService = exports.FleetDataService = function FleetDataService() {
+    _classCallCheck(this, FleetDataService);
+
+    this.cars = [];
+    this.drones = [];
+};
+//# sourceMappingURL=fleetDataService.js.map
---
+++ src/services/fleetDataService.js
@@ -0,0 +1,10 @@
+import {Car} from "../classes/Car.js";
+import {Drone} from "../classes/Drone.js";
+
+export class FleetDataService{
+
+    constructor(){
+        this.cars = [];
+        this.drones = [];
+    }
+}
